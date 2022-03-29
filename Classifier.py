21a95fbde964328ed0ab817a46a3f4e398be5186
Anirudh Rao <anirudh131299@gmail.com>
2022-03-29 20:35:49 +0530

    Classifer

--- Classifier.py
+++ Classifier.py
@@ -2,6 +2,10 @@ import streamlit as st
 import numpy as np
 import pandas as pd
 from sklearn import datasets
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.svm import SVC
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
 
 
 st.title("Classifier Example")
@@ -34,19 +38,39 @@ st.write("Shape of the data",X.shape)
 st.write("Number of Classes", len(np.unique(y)))
 
 #Adding Parameters to the Classifier
-def AddParameters(classifierName):
+def add_parameter_ui(clf_name):
     params = dict()
-    if classifierName=="KNN":
-        K = st.sidebar.slider("K",1,15)
-        params["K"]=K
-    elif classifierName=="SVM":
-        C = st.sidebar.slider("C",0.01,10.0)
-        params["C"]=C
+    if clf_name == 'SVM':
+        C = st.sidebar.slider('C', 0.01, 10.0)
+        params['C'] = C
+    elif clf_name == 'KNN':
+        K = st.sidebar.slider('K', 1, 15)
+        params['K'] = K
     else:
-        max_depth = st.sidebar.slider("MaxDepth",2,15)
-        NumberEst = st.sidebar.slider("Number of estimators",1,100)
-        params["Max_depth"]=max_depth
-        params["NEstimators"]=NumberEst
+        max_depth = st.sidebar.slider('max_depth', 2, 15)
+        params['max_depth'] = max_depth
+        n_estimators = st.sidebar.slider('n_estimators', 1, 100)
+        params['n_estimators'] = n_estimators
     return params
 
-AddParameters(classifierName)
+params = add_parameter_ui(classifierName)
+
+def GetClassifier(clf_name, params):
+    clf = None
+    if clf_name == 'SVM':
+        clf = SVC(C=params['C'])
+    elif clf_name == 'KNN':
+        clf = KNeighborsClassifier(n_neighbors=params['K'])
+    else:
+        clf = clf = RandomForestClassifier(n_estimators=params['n_estimators'], 
+            max_depth=params['max_depth'], random_state=1234)
+    return clf
+
+
+clf =  GetClassifier(classifierName,params)
+
+#Building the Classifier
+X_train, X_test, y_train, y_test =  train_test_split(X,y,test_size=0.2,random_state=1234)
+
+clf.fit(X_train,y_train)
+y_pred = clf.predict(X_test)
